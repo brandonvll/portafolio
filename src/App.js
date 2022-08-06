5bf15b5181c866cf53a86120fb5a7681d6c9fef0
brandonvll <98933526+brandonvll@users.noreply.github.com>
2022-08-05 22:18:58 -0500

    up code
    up code

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         <Skills />
         <Briefcase />
         <Contact />
-        <Footer/>
+        <Footer />
       </div>
     </>
   );
--- src/components/Briefcase.js
+++ src/components/Briefcase.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
+import clima from "../images/clima.PNG";
+import rickandmorty from "../images/rickandmorty.PNG";
+import pokeapi from "../images/pokeapi.PNG";
+import frases from "../images/frases.PNG";
 
 const Briefcase = () => {
-    return (
-        <div>
-            <h1>My Works</h1>
+  return (
+    <div>
+      <section class="portfolio-section">
+        <div class="portfolio-container">
+          <article class="portfolio-title">
+            <h3 className="title-works">My Work</h3>
+          </article>
+          <article class="portfolio-inner">
+            <ul className="list-works">
+              <li>
+                <figure class="portfolio-img-container">
+                  <img className="img-works" src={clima} alt="Clima" />
+                </figure>
+              </li>
+              <li className="img-works">
+                <figure class="portfolio-img-container">
+                  <img className="img-works" src={pokeapi} alt="Poke-Api" />
+                </figure>
+              </li>
+              <li className="img-works">
+                <figure class="portfolio-img-container">
+                  <img
+                    className="img-works"
+                    src={rickandmorty}
+                    alt="Rick-and.Morty"
+                  />
+                </figure>
+              </li>
+              <li className="img-works">
+                <figure class="portfolio-img-container">
+                  <img className="img-works" src={frases} alt="phrases" />
+                </figure>
+              </li>
+            </ul>
+          </article>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default Briefcase;
